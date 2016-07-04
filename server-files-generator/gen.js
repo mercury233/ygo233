d386be4fdc320d18f5fa32dda792f830a4f4c244
mercury233 <me@mercury233.me>
2016-07-05 03:10:34 +0800

    update gen
    pre-release
    filesize

--- server-files-generator/gen.js
+++ server-files-generator/gen.js
@@ -17,6 +17,14 @@ var hash = function(relpath, fullpath) {
   return h;
 }
 
+var size = function(relpath, fullpath) {
+  if (!fullpath) {
+    fullpath = config.base_path + relpath;
+  }
+  var s = fs.statSync(fullpath).size;
+  return s;
+}
+
 spawnSync(config.rm_exe, config.rm_cmd_base, { cwd: config.base_path, env: process.env });
 spawnSync(config.rm_exe, config.rm_cmd_server, { cwd: config.server_path, env: process.env });
 
@@ -33,6 +41,7 @@ for (var i in ygopro_files.files) {
   var file = ygopro_files.files[i];
   var item = {};
   item.name = file;
+  item.size = size(file);
   item.hash = hash(file);
   files_json.files.push(item);
 }
@@ -51,6 +60,7 @@ for (var i in ygopro_files.folders) {
       console.log(filepath);
       var item = {};
       item.name = file;
+      item.size = size(filepath);
       item.hash = hash(filepath);
       folder_hashes.push(item);
     }
@@ -60,6 +70,46 @@ for (var i in ygopro_files.folders) {
 
 fs.writeFileSync(config.server_path+"files.json", JSON.stringify(files_json));
 
+//先行卡文件
+var pre_json = {};
+var pre_pic_files = [];
+
+pre_json.files=[];
+
+for (var i in ygopro_files.pre_files) {
+  var file = ygopro_files.pre_files[i];
+  var item = {};
+  item.name = file;
+  item.size = size(file, config.pre_path + file);
+  item.hash = hash(file, config.pre_path + file);
+  pre_json.files.push(item);
+}
+
+//需要检查这些文件夹里所有的文件
+pre_json.folders={};
+
+for (var i in ygopro_files.pre_folders) {
+  var folder = ygopro_files.pre_folders[i];
+  var folder_hashes = [];
+  var folder_files=fs.readdirSync(config.pre_path+folder);
+  for (var j in folder_files) {
+    var file = folder_files[j];
+    if (file.indexOf(".")>0) { //文件必然有扩展名
+      var filepath=folder + "\\" + file;
+      console.log(filepath);
+      var item = {};
+      item.name = file;
+      item.size = size(filepath, config.pre_path + filepath);
+      item.hash = hash(filepath, config.pre_path + filepath);
+      folder_hashes.push(item);
+      pre_pic_files.push(filepath);
+    }
+  }
+  pre_json.folders[folder] = folder_hashes;
+}
+
+fs.writeFileSync(config.server_path+"pre.json", JSON.stringify(pre_json));
+
 var ygo233_json = {};
 
 ygo233_json.files=[];
@@ -68,6 +118,7 @@ for (var i in ygopro_files.ygo233_files) {
   var file = ygopro_files.ygo233_files[i];
   var item = {};
   item.name = file;
+  item.size = size(file);
   item.hash = hash(file);
   ygo233_json.files.push(item);
 }
@@ -94,6 +145,24 @@ db_item.filecount=1;
 db_item.filesize=fs.statSync(config.base_path+"db.7z").size;
 db_item.files=["cards.cdb"];
 packages_array.push(db_item);
+
+spawnSync(config.sevenzip_exe, ["a", "pre.7z", "expansions\\pre-release.cdb", "deck", "script"], { cwd: config.pre_path, env: process.env });
+var pre_item={};
+pre_item.name="先行卡数据库";
+pre_item.filename="pre.7z";
+pre_item.filecount=1;
+pre_item.filesize=fs.statSync(config.pre_path+"pre.7z").size;
+pre_item.files=["expansions\\pre-release.cdb"];
+packages_array.push(pre_item);
+
+spawnSync(config.sevenzip_exe, ["a", "pre_pic.7z", "pics"], { cwd: config.pre_path, env: process.env });
+var pre_pic_item={};
+pre_pic_item.name="先行卡卡图包";
+pre_pic_item.filename="pre_pic.7z";
+pre_pic_item.filecount=pre_pic_files.length;
+pre_pic_item.filesize=fs.statSync(config.pre_path+"pre_pic.7z").size;
+pre_pic_item.files=pre_pic_files;
+packages_array.push(pre_pic_item);
 
 var ygo233_7z_arg=["a", "ygo233_update.7z"].concat(ygopro_files.ygo233_files);
 spawnSync(config.sevenzip_exe, ygo233_7z_arg, { cwd: config.base_path, env: process.env });
@@ -153,6 +222,8 @@ packages_json.packages=packages_array;
 fs.writeFileSync(config.server_path+"packages.json", JSON.stringify(packages_json));
 
 spawnSync(config.mv_exe, config.mv_cmd, { cwd: config.base_path, env: process.env });
+
+spawnSync(config.mv_exe, config.mv_cmd, { cwd: config.pre_path, env: process.env });
 
 spawnSync(config.sevenzip_exe, ["a", "data.7z", "*.json"], { cwd: config.server_path, env: process.env });
 
